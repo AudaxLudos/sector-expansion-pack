b023712bb2f7646989f16515db84969d0b19dd57
AudaxLudos <james.tarrobal.777@gmail.com>
2025-04-09 15:15:38 +0800

    Add custom hub mission implementation

---
+++ src/sectorexpansionpack/missions/hub/SEPHubMission.java
@@ -0,0 +1,26 @@
+package sectorexpansionpack.missions.hub;
+
+import com.fs.starfarer.api.campaign.econ.Industry;
+import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.impl.campaign.missions.hub.HubMissionWithBarEvent;
+
+public class SEPHubMission extends HubMissionWithBarEvent {
+    @Override
+    protected boolean create(MarketAPI createdAt, boolean barEvent) {
+        return false;
+    }
+
+    public void requireMarketHasInstalledItems() {
+        this.search.marketReqs.add(new MarketRequirement() {
+            @Override
+            public boolean marketMatchesRequirement(MarketAPI market) {
+                for (Industry ind : market.getIndustries()) {
+                    if (!ind.getVisibleInstalledItems().isEmpty()) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+}
