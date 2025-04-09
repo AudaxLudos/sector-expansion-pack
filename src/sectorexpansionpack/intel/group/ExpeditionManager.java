4439abd66600aec3903657ac5c8ba3123e96ce4b
AudaxLudos <james.tarrobal.777@gmail.com>
2025-04-09 13:13:12 +0800

    Update base interval for next event trigger

--- src/sectorexpansionpack/intel/group/ExpeditionManager.java
+++ src/sectorexpansionpack/intel/group/ExpeditionManager.java
@@ -5,6 +5,8 @@ import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.impl.campaign.intel.BaseEventManager;
 
 public class ExpeditionManager extends BaseEventManager {
+    public static float EVENT_INTERVAL = 60f; // In days
+
     public static void register() {
         if (!Global.getSector().hasScript(ExpeditionManager.class)) {
             Global.getSector().addTransientScript(new ExpeditionManager());
@@ -19,6 +21,11 @@ public class ExpeditionManager extends BaseEventManager {
     @Override
     protected int getMaxConcurrent() {
         return 3;
+    }
+
+    @Override
+    protected float getBaseInterval() {
+        return EVENT_INTERVAL;
     }
 
     @Override
