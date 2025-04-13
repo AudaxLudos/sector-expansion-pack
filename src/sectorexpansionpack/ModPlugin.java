09bdbbfed343d54b093b45c55106604b1a1e6668
AudaxLudos <james.tarrobal.777@gmail.com>
2025-04-13 12:12:42 +0800

    Add mod files

---
+++ src/sectorexpansionpack/ModPlugin.java
@@ -0,0 +1,10 @@
+package sectorexpansionpack;
+
+import com.fs.starfarer.api.BaseModPlugin;
+
+public class ModPlugin extends BaseModPlugin {
+    @Override
+    public void onApplicationLoad() throws Exception {
+        throw new Exception("Sector Expansion Pack mod is working");
+    }
+}
