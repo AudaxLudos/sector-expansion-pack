8b9cdc97aca22aa13c8a199e77834cc4fef7df64
AudaxLudos <james.tarrobal.777@gmail.com>
2026-02-20 01:25:36 +0800

    Rebalance standard repairs skill Reduce repair rate outside of combat multiplier to 30% from 40% Reduce combat readiness flat recovery rate modifier to 4% from 5%

--- src/sectorexpansionpack/skills/sic/purist/StandardRepairs.java
+++ src/sectorexpansionpack/skills/sic/purist/StandardRepairs.java
@@ -13,8 +13,8 @@ import java.awt.*;
 import java.util.Objects;
 
 public class StandardRepairs extends SCBaseSkillPlugin {
-    public static float REPAIR_RATE_PER_DAY_MULT = 0.40f;
-    public static float CR_RECOVERY_RATE_PER_DAY_MOD = 0.05f;
+    public static float REPAIR_RATE_PER_DAY_MULT = 0.30f;
+    public static float CR_RECOVERY_RATE_PER_DAY_MOD = 0.04f;
 
     @Override
     public String getAffectsString() {
