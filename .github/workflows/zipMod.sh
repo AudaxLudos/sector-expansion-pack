abddf66d6e0a9ba0b9fee8e7d17e542f4e83b82d
AudaxLudos <james.tarrobal.777@gmail.com>
2026-03-27 22:14:13 +0800

    Add github action release script

---
+++ .github/workflows/zipMod.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+folderToZip="./../.."
+startingDir=$(pwd)
+
+echo "Executing dir: $startingDir"
+echo "Location of folder to zip:" "$(realpath $folderToZip)"
+
+cd $folderToZip || return
+
+# Get the mod version and mod name from mod_info.json
+json=$(cat mod_info.json)
+modVersion=$(echo "$json" | jq -r '.version')
+modName=$(echo "$json" | jq -r '.name')
+
+# Set zip name and folder name
+outputFolderName="$modName $modVersion"
+zipName="$(echo "$modName" | sed 's/ //g').zip"
+
+# Recreate the temp folder if it happens to be present.
+rm -rf "./$outputFolderName"
+mkdir "$outputFolderName"
+
+# 1. List all files in git, which uses gitignore.
+# 2. Remove any file matching the blacklist.
+# 3. Copy to a new folder with the mod name.
+printf "Reading blacklist...\n%s" "$(cat "$startingDir"/blacklist.txt)"
+git ls-files | grep -Evf "$startingDir"/blacklist.txt | while read -r file; do cp --parents "$file" "$outputFolderName"; done
+
+# Zip the folder, then clean it up.
+zip -r "$zipName" "./$outputFolderName"
+echo "Created zip file at $(realpath "$zipName")"
+rm -rf "./$outputFolderName"
+
+# Move the zip to the artifacts folder in the executing directory.
+mkdir -p "$startingDir"/artifacts
+mv ./"$zipName" "$startingDir"/artifacts/
+echo "Moved zip file to $(realpath "$startingDir"/artifacts/"$zipName")"
+
+# Return to the executing directory.
+cd "$startingDir" || return
